8493386846c8c0686fb8b014cb13df2a9b4e7bf3
prabin tp <prathazh@publicisgroupe.net>
2025-05-23 11:39:35 +0400

    feat(aem): universal item fix

--- components/image-with-cta.tsx
+++ components/image-with-cta.tsx
@@ -22,7 +22,7 @@ const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
   return (
     <div
       className="flex flex-col md:flex-row items-center gap-8 my-12"
-      data-aue-type="section"
+      data-aue-type="Content With Image"
       data-aue-resource={`urn:aemconnection:${cfPath}/jcr:content/data/master`}
     >
       {isImageLeft && (
@@ -31,15 +31,16 @@ const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
             src={imageUrl}
             alt={imageAlt}
             className="rounded-2xl shadow-lg w-full object-cover"
+            data-aue-prop="componentImage"
           />
         </div>
       )}
 
       <div className="md:w-1/2 text-center md:text-left">
-        <h2 className="text-2xl font-bold text-gray-900 mb-4">
+        <h2 data-aue-prop="title" data-aue-type="text" className="text-2xl font-bold text-gray-900 mb-4">
           {title}
         </h2>
-        <p className="text-gray-600 mb-6" >
+        <p className="text-gray-600 mb-6" data-aue-prop="description1" data-aue-type="text" >
           {description1}
         </p>
        
