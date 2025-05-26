08a69fc4cf01c5b5d96d8eaec21960824d7ad9c1
prabin tp <prathazh@publicisgroupe.net>
2025-05-26 18:52:52 +0400

    feat(aem): spotlight comp

--- components/image-with-cta.tsx
+++ components/image-with-cta.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Spotlight from './spotlight';
 
 export interface ImageWithCTAProps {
   title: string;
   description1: any;
   imageUrl: string;
   imageAlt?: string;
-  variation?: 'left' | 'right';
-  cfPath: string; // Content Fragment path for AEM UE
+  variation?: 'left' | 'right' | 'background'; 
+  cfPath: string;
 }
 
 const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
@@ -14,14 +15,22 @@ const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
   description1,
   imageUrl,
   imageAlt = '',
-  variation = 'left',
+  variation = 'background',
   cfPath,
 }) => {
   const isImageLeft = variation === 'left';
 
   return (
+    variation === 'background' ? (
+      <Spotlight
+      title= {title}
+    description= {description1}
+imageUrl={imageUrl}
+cfPath={cfPath}
+/>
+    ) :
     <div
-      className="flex flex-col md:flex-row items-center gap-8 my-12"
+      className="flex flex-col md:flex-row items-center gap-8 my-12 container mx-auto"
       data-aue-type="Content With Image"
       data-aue-resource={`urn:aemconnection:${cfPath}/jcr:content/data/master`}
     >
@@ -30,14 +39,14 @@ const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
           <img
             src={imageUrl}
             alt={imageAlt}
-            className="rounded-2xl shadow-lg w-full object-cover"
+            className=" shadow-lg w-full object-cover"
             data-aue-prop="componentImage"
           />
         </div>
       )}
 
       <div className="md:w-1/2 text-center md:text-left">
-        <h2 data-aue-prop="title" data-aue-type="text" className="text-2xl font-bold text-gray-900 mb-4">
+        <h2 data-aue-prop="title" data-aue-type="text" className="text-4xl font-medium text-gray-900 mb-4">
           {title}
         </h2>
         <p className="text-gray-600 mb-6" data-aue-prop="description1" data-aue-type="text" >
@@ -51,7 +60,7 @@ const ImageWithCTA: React.FC<ImageWithCTAProps> = ({
           <img
             src={imageUrl}
             alt={imageAlt}
-            className="rounded-2xl shadow-lg w-full object-cover"
+            className="shadow-lg w-full object-cover"
           />
         </div>
       )}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,57 @@
 import ImageWithCTA from "@/components/image-with-cta";
+import Header from '@/components/header';
+import Footer from '@/components/footer';
+
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaXTwitter,
+  FaGithub,
+  FaYoutube,
+} from "react-icons/fa6";
+
+const footerSections = [
+  {
+    title: "Hospitality",
+    links: [
+      { label: "Offices", href: "#" },
+      { label: "Retails", href: "#" },
+      { label: "Hotels", href: "#" },
+      { label: "Residence", href: "#" }
+    ],
+  },
+  {
+    title: "Diriyah",
+    links: [
+      { label: "News", href: "#" },
+      { label: "Media", href: "#" },
+      { label: "Events", href: "#" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { label: "About", href: "#" },
+      { label: "Jobs", href: "#" },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      { label: "Terms of service", href: "#" },
+      { label: "Privacy policy", href: "#" },
+      { label: "License", href: "#" },
+    ],
+  },
+];
+
+const socialIcons = [
+  { icon: FaFacebookF, href: "#" },
+  { icon: FaInstagram, href: "#" },
+  { icon: FaXTwitter, href: "#" },
+  { icon: FaGithub, href: "#" },
+  { icon: FaYoutube, href: "#" },
+];
 
 
 interface HomeProps {
@@ -14,23 +67,17 @@ export default function Home({ content }: HomeProps) {
 
 const { title, descrpition, componentFragmentReference } = content || {};
   return (
+    <div>
+       <Header />
     <div
-      className={` grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+      className={`items-center justify-items-center mx-auto`}
     >
-
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start" 
+      <main className=" w-full flex flex-col gap-[32px] row-start-2 items-center sm:items-start" 
       data-aue-type="page"
       data-aue-resource={`urn:aemconnection:${content?._path}/jcr:content/data/master`}
-      >
-      <h1
-        data-aue-prop="title"
-        data-cf-field="title"
-        className="text-4xl font-bold text-gray-900"
 
-        >{title}</h1>
-      <p className="text-2xl" data-aue-prop="descrpition" data-aue-type="text" data-aue-label="description"> 
-        {descrpition?.plaintext}
-      </p>
+      >
+    
       {componentFragmentReference && componentFragmentReference.map((component, index) => (
         <ImageWithCTA
           key={component.id || index}
@@ -38,15 +85,40 @@ const { title, descrpition, componentFragmentReference } = content || {};
           description1={component.description1?.plaintext ?? ''}
           imageUrl={component.componentImage?.image?._publishUrl ?? ''}
           imageAlt={component.componentImage?.imageAltText ?? ''}
-          variation={index % 2 === 0 ? 'left' : 'right'}
+          variation={index === 0 ? "background" : index % 2 === 0 ? 'left' : 'right'}
           cfPath={component._path}
         />
       ))}
+
+
+<div className="container mx-auto flex flex-col items-center gap-3.5 justify-center pb-12" data-aue-type="page"
+      data-aue-resource={`urn:aemconnection:${content?._path}/jcr:content/data/master`}>
+<h1
+        data-aue-prop="title"
+        data-aue-type="text"  
+        data-cf-field="title"
+        className="text-4xl font-bold text-gray-900"
+
+        >{title}</h1>
+      <p className="text-2xl" data-aue-prop="descrpition" data-aue-type="text" data-aue-label="description"> 
+        {descrpition?.plaintext}
+      </p>
+        
+
+</div>
+   
        
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-       
-      </footer>
+     
+     
+     
+    </div>
+    <Footer
+      logo={<span className="text-indigo-600 text-3xl font-bold">〰</span>}
+      sections={footerSections}
+      socialLinks={socialIcons}
+      copyright="© 2024 Diriyah, Inc. All rights reserved."
+    />
     </div>
   );
 }
@@ -68,7 +140,6 @@ export async function getServerSideProps() {
       notFound: true,
     };
   }
-  console.log("GraphQL response:", json);
 
   const content = json?.data?.diriyahPageContentFragmentModelByPath?.item || null;
 
