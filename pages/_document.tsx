9885969b1669adb5bd39345cf0b36e69dfdfa314
LL\dhakumar16 <dhananjaya.kumar@publicissapient.com>
2025-06-25 14:55:34 +0530

    creating the new demo page with new aem aws server grapghql json

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <script src="https://universal-editor-service.adobe.io/cors.js" async></script>
-      <meta name="urn:adobe:aue:system:aemconnection" content={`aem:${process.env.AEM_HOST}`}></meta>
+      <meta name="urn:adobe:aue:system:aemconnection" content={`aem65:${process.env.AEM_ON_PREM_HOST}`}></meta>
+      <meta name="urn:adobe:aue:config:service" content="https://localhost:8008" />
         </Head>
       <body className="antialiased">
         <Main />
